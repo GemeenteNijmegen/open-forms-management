4d836b8e7eeec525781aaac17f4ce4bb6250e4f4
WietekeNijmegen <wietekenijmegen@gmail.com>
2025-12-14 22:38:44 +0100

    chore: first commit repo standard build

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,12 +3,12 @@ const project = new GemeenteNijmegenCdkApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   devDeps: ['@gemeentenijmegen/projen-project-type'],
-  makeSampleFiles: true,
   name: 'open-forms-management',
   projenrcTs: true,
   repository: 'https://github.com/GemeenteNijmegen/open-forms-management',
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    '@gemeentenijmegen/aws-constructs',
+  ], /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
