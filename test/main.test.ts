54898583eaff7a99f46301834d7c50f32a74ffc9
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2026-01-06 13:57:31 +0100

    feat: Add config construct
    Adds the config construct to the app.
    Also sets isolatedModules: true in tsconfig, to speed up jest.

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,3 +1,22 @@
-test('test', () => {
-  console.log('No tests yet...');
+import { Criticality } from '@gemeentenijmegen/aws-constructs';
+import { Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { AppStack } from '../src/AppStack';
+
+describe('AppStack', () => {
+  let stack: Stack;
+  const config = { branchName: 'test', buildEnvironment: { account: 'test', region: 'eu-central-1' }, deploymentEnvironment: { account: 'test', region: 'eu-central-1' }, criticality: new Criticality('low') };
+
+  beforeEach(() => {
+    stack = new AppStack(new Stack(), 'TestStack', { configuration: config });
+  });
+
+  it('should create a stack', () => {
+    expect(stack).toBeDefined();
+  });
+
+  it('should have a ConfigTable resource', () => {
+    const template = Template.fromStack(stack);
+    template.resourceCountIs('AWS::DynamoDB::Table', 1);
+  });
 });
