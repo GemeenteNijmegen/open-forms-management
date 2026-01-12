1d72a54a7c2432215a8fbc003e3972078903c7f0
WietekeNijmegen <wietekenijmegen@gmail.com>
2026-01-12 17:14:33 +0100

    chore: nodeversion echo

--- src/PipelineStack.ts
+++ src/PipelineStack.ts
@@ -64,6 +64,8 @@ export class PipelineStack extends Stack {
         BRANCH_NAME: this.props.configuration.branchName,
       },
       commands: [
+        'echo "Node version before n lts:"',
+        'node -v',
         'n lts',
         'yarn install --frozen-lockfile',
         'npx projen build',
